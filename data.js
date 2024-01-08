8a4f1bed6f1cdc2fadba4d2fb20276fa43eca4c5
Pascal Thelke <p.thelke@googlemail.com>
2024-01-08 18:27:39 +0100

    jsonArray seperated + colored aswers

---
+++ data.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Tim Berners-Lee",
+        "answer_3": "Robert Kotick",
+        "answer_4": "Hakon Wium Lie",
+        "right_answer": 2,
+        
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um einen Hyperlink zu erstellen?",
+        "answer_1": " <link>",
+        "answer_2": " <a>",
+        "answer_3": " <href>",
+        "answer_4": " <url>",
+        "right_answer": 2,
+    },
+    {
+        "question": "Was bedeutet die Abkürzung HTML?",
+        "answer_1": "Hypertext Markup Language",
+        "answer_2": "Hyperlink and Text Markup Language",
+        "answer_3": "High Tech Modern Language",
+        "answer_4": "Home Tool Markup Language",
+        "right_answer": 1,
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um eine geordnete Liste zu erstellen?",
+        "answer_1": " <ul>",
+        "answer_2": " <ol>",
+        "answer_3": " <li>",
+        "answer_4": " <dl>",
+        "right_answer": 2,
+    },
+    {
+        "question": "Was ist der Zweck des HTML-Elements ' <head>'?",
+        "answer_1": "Es enthält den Hauptinhalt der Webseite.",
+        "answer_2": "Es definiert den Header-Bereich der Webseite.",
+        "answer_3": "Es enthält Metainformationen über das Dokument.",
+        "answer_4": "Es stellt multimediale Inhalte dar.",
+        "right_answer": 3,
+    },
+];
--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-let questions = [
-    {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Tim Berners-Lee",
-        "answer_3": "Robert Kotick",
-        "answer_4": "Hakon Wium Lie",
-        "right_answer": 2,
-        
-    },
-    {
-        "question": "Welches HTML-Element wird verwendet, um einen Hyperlink zu erstellen?",
-        "answer_1": " <link>",
-        "answer_2": " <a>",
-        "answer_3": " <href>",
-        "answer_4": " <url>",
-        "right_answer": 2,
-    },
-    {
-        "question": "Was bedeutet die Abkürzung HTML?",
-        "answer_1": "Hypertext Markup Language",
-        "answer_2": "Hyperlink and Text Markup Language",
-        "answer_3": "High Tech Modern Language",
-        "answer_4": "Home Tool Markup Language",
-        "right_answer": 1,
-    },
-    {
-        "question": "Welches HTML-Element wird verwendet, um eine geordnete Liste zu erstellen?",
-        "answer_1": " <ul>",
-        "answer_2": " <ol>",
-        "answer_3": " <li>",
-        "answer_4": " <dl>",
-        "right_answer": 2,
-    },
-    {
-        "question": "Was ist der Zweck des HTML-Elements ' <head>'?",
-        "answer_1": "Es enthält den Hauptinhalt der Webseite.",
-        "answer_2": "Es definiert den Header-Bereich der Webseite.",
-        "answer_3": "Es enthält Metainformationen über das Dokument.",
-        "answer_4": "Es stellt multimediale Inhalte dar.",
-        "right_answer": 3,
-    },
-];
-
 let currentQuestion = 0;
 
 function init(){
@@ -67,8 +23,10 @@ function choosenAnswer(selected){ // selected wird als Eingabeparameter per onli
 
     if(selectedQuestionNumber == question['right_answer']){ // wenn die ausgewählte Nummer der Anwort mit der Zahl in right answer übereinstimmt 
         console.log('right answer!')// ausgabe
+        document.getElementById(selected).parentNode.classList.add('bg-success');
     } else{ //anderfalls
         console.log('wrong answer!')// ausgabe
+        document.getElementById(selected).parentNode.classList.add('bg-danger');
     }
 
 }
