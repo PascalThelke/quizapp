85b7ec4ba6c8bc7556e8ede4f875fd0ffe84c825
Pascal Thelke <p.thelke@googlemail.com>
2024-01-08 17:54:51 +0100

    commentary included

--- script.js
+++ script.js
@@ -45,11 +45,11 @@ let questions = [
 let currentQuestion = 0;
 
 function init(){
-    document.getElementById('total-questions').innerHTML = questions.length;
+    document.getElementById('total-questions').innerHTML = questions.length; // zeigt an wie viele Fragen insgesammt
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion(){ // rendert Frage und Antwordmöglichkeiten
     let actual = questions[currentQuestion];
     document.getElementById('question_text').innerHTML = actual['question'];
     document.getElementById('answer_1').innerHTML = actual['answer_1'];
@@ -58,17 +58,17 @@ function showQuestion(){
     document.getElementById('answer_4').innerHTML = actual['answer_4'];
 }
 
-function choosenAnswer(selected){
-    let question = questions[currentQuestion];
-    console.log('Seleceted answer is', selected);
-    let selectedQuestionNumber = selected.slice(-1);
-    console.log('Selected number is', selectedQuestionNumber);
-    console.log('Current question', question['right_answer']);
+function choosenAnswer(selected){ // selected wird als Eingabeparameter per onlick übergeben
+    let question = questions[currentQuestion]; // question wird definiert indem die 0 stelle des Arrays aufgerufen wird
+    console.log('Seleceted answer is', selected); // ausgabe der ausgewühlten anwort
+    let selectedQuestionNumber = selected.slice(-1); // hier wird die letzte stelle des strings deklariert
+    console.log('Selected number is', selectedQuestionNumber); // ausgabe der nummer der gegebenen Antwort
+    console.log('Current question', question['right_answer']);// ausgabe der aktuellen der richtigen Antwort
 
-    if(selectedQuestionNumber == question['right_answer']){
-        console.log('right answer!')
-    } else{
-        console.log('wrong answer!')
+    if(selectedQuestionNumber == question['right_answer']){ // wenn die ausgewählte Nummer der Anwort mit der Zahl in right answer übereinstimmt 
+        console.log('right answer!')// ausgabe
+    } else{ //anderfalls
+        console.log('wrong answer!')// ausgabe
     }
 
 }
