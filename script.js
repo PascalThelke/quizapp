c2085f179087034d4e07ba9dc9581ad03bc03078
Pascal Thelke <p.thelke@googlemail.com>
2024-01-09 15:58:03 +0100

    endscreen include score now

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let selected; // wird befüllt um die ausgwählte antwort in anderen funktionen 
 let idOfRightLetterbox;
 let idOfPickedLetterbox;
 let idOfRightAnswer;
+let rightQuestions = 0;
 
 
 function init() {
@@ -33,21 +34,18 @@ function showQuestion() { // rendert Frage und Antwortmöglichkeiten
 function choosenAnswer(selectedAnswer) { // selected wird als Eingabeparameter per onlick übergeben
     let question = questions[currentQuestion]; // question wird definiert indem die 0 stelle des Arrays aufgerufen wird
     selected = selectedAnswer;
-    console.log('Seleceted answer is', selected); // ausgabe der ausgewühlten anwort
     let selectedQuestionNumber = selected.slice(-1); // hier wird die letzte stelle des strings deklariert
-    console.log('Selected number is', selectedQuestionNumber); // ausgabe der nummer der gegebenen Antwort
-    console.log('Current question', question['right_answer']);// ausgabe der aktuellen der richtigen Antwort
-
+    
     idOfRightAnswer = `answer_${question['right_answer']}`; // nimmt sich die nummer aus dem array, welche der ID der Anwort entspricht
     idOfRightLetterbox = `letterbox_${question['right_answer']}`; // bestimmt die id der dazugehörigen letterbox
     idOfPickedLetterbox = `letterbox_${selectedQuestionNumber}`;    // bestimmt die id der letterbox in der ausgewählten Antwort
 
     if (selectedQuestionNumber == question['right_answer']) { // wenn die ausgewählte Nummer der Anwort mit der Zahl in right answer übereinstimmt 
-        console.log('right answer!')// ausgabe
         document.getElementById(selected).parentNode.classList.add('succeeded-answer-box'); // greift auf das parent element zu und gibt die klasse succeeded-answer..
         document.getElementById(idOfRightLetterbox).classList.add('bg-success'); // greift auf die Letterbox der richtigen Anwort zu und gibt die klasse bg-success
+        rightQuestions++; // erhöht den counter für richtige anworten um 1
+        document.getElementById('right-question-amount').innerHTML = rightQuestions;
     } else { //anderfalls
-        console.log('wrong answer!')// ausgabe
         document.getElementById(selected).parentNode.classList.add('failed-answer-box'); // greift auf das parent element zu und gibt die klasse failed answer..
         document.getElementById(idOfPickedLetterbox).classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('succeeded-answer-box'); // makiert die richtige Antwort bei falscher Auswahl
@@ -57,6 +55,7 @@ function choosenAnswer(selectedAnswer) { // selected wird als Eingabeparameter p
 
 function nextQuestion() {
     currentQuestion++; // wir von Stelle 0 auf 1 im Array erhöht
+
     document.getElementById('gonext-button').disabled = true;
     resetAnswers();
     showQuestion();
